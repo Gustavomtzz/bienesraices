61e46754344bc886211fec6d050ac1775e9588a8
Gustavomtzz <147465331+Gustavomtzz@users.noreply.github.com>
2023-11-01 21:39:28 -0300

    Update layout.php
    Update Footer Nav

--- views/layout.php
+++ views/layout.php
@@ -60,10 +60,10 @@ if (!isset($inicio)) {
     <footer class="site-footer seccion">
         <div class="contenedor contenedor-footer">
             <nav class="navegacion">
-                <a href="/nosotros.php">Nosotros</a>
-                <a href="/anuncios.php">Anuncios</a>
-                <a href="/blog.php">Blog</a>
-                <a href="/contacto.php">Contacto</a>
+                <a href="/nosotros">Nosotros</a>
+                <a href="/anuncios">Anuncios</a>
+                <a href="/blog">Blog</a>
+                <a href="/contacto">Contacto</a>
             </nav>
             <p class="copyright">Todos los Derechos Reservados <?php echo date('Y'); ?> &copy; </p>
         </div>
